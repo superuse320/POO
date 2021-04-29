274e1c0e8b94624de0dd50cbf422a71d9bd7b6a4
superuse320 <83409382+superuse320@users.noreply.github.com>
2021-04-29 15:08:00 -0400

    Update Empleado.py

--- Empleado.py
+++ Empleado.py
@@ -1,9 +1,10 @@
-
 class Empleado:
-    def __init__(self,nombre):
-        self.__nombre=nombre
+    def __init__(self,nombre,apellido,direccion,ci):
+        self.nombre=nombre
+        self.apellido=apellido
+        self.direccion=direccion
+        self.ci=ci
+
     def __str__(self):
-        dato="Nombre:"+self.__nombre
-        return dato
-    def getnombre(self):
-        return self.__nombre          
+        dato="Nombre:"+self.nombre+"\nApellido "+self.apellido+"\nDireccion "+self.direccion+"\nCi "+self.ci
+        return dato      
