4d9b7cda69db2d81dfa29eabc6d5692cbc4da4a0
superuse320 <83409382+superuse320@users.noreply.github.com>
2021-04-29 18:57:41 -0400

    Add files via upload

--- Empleado.py
+++ Empleado.py
@@ -1,10 +1,33 @@
-class Empleado:
-    def __init__(self,nombre,apellido,direccion,ci):
-        self.nombre=nombre
-        self.apellido=apellido
-        self.direccion=direccion
-        self.ci=ci
 
+class Empleado:
+    def __init__(self,nombre,apellido,direccion,ci,fecha):
+        self.__nombre=nombre
+        self.__apellido=apellido
+        self.__direccion=direccion
+        self.__ci=ci
+        self.__fecha=fecha
     def __str__(self):
-        dato="Nombre:"+self.nombre+"\nApellido "+self.apellido+"\nDireccion "+self.direccion+"\nCi "+self.ci
-        return dato      
+        
+        return "Nombre:"+self.__nombre+"\nApellido "+self.__apellido+"\nDireccion "+self.direccion__+"\nCi "+self.__ci +"\nFecha"+self.__fecha
+        
+    def getNombre(self):
+        return self.__nombre
+    def getApellido(self):
+        return self.__apellido
+    def getDireccion(self):
+        return self.__direccion
+    def getCi(self):
+        return self.__ci
+    def getFecha(self):
+        return self.__fecha
+
+    def setNombre(self,nombre):
+        self.__nombre=nombre
+    def setApellido(self,apellido):
+        self.__apellido=apellido
+    def setDireccion(self,direccion):
+        self.__direccion=direccion
+    def setCi(self,ci):
+        self.__ci=ci
+    def setFecha(self,fecha):
+        self.__fecha=fecha   
---
+++ testEmpleado.py
@@ -0,0 +1,128 @@
+from Empleado import Empleado
+import os
+def menu():
+                                                                                                                        
+    os.system("cls")
+   
+    print("\t.......................")
+    print("\t Menu Empleado")
+    print("\t.......................")
+    print("\t① Registrar Empleado")
+    print("\t② Eliminar Empleado") 
+    print("\t③ Listar Empleado")
+    print("\t④ Actualiza Empleado")
+    print("\t⓪. Salir ")
+
+def Registrar(listaEmpleados):
+    #os.system("cls")
+
+    nombre=input("Nombre: ")
+    apellido=input("Apellido")
+    direccion=input("Direccion:")
+    ci=input("Ci:")
+    fecha=input("Fecha: ")
+    empleado=Empleado(nombre,apellido,direccion,ci,fecha)
+    listaEmpleados.append(empleado)
+def MenuEliminarEmpleado():
+    print("1.Eliminar por # Empleado")    
+    print("2.Eliminar por Nombre de Empleado")    
+    print("3.Eliminar por ci de Empleado")
+    print("0 Csncelar")
+def EliminacionCi(Ci,listaempleados):
+    for i in range(len(listaempleados)):
+        if Ci==listaempleados[i].getCi():
+            del listaempleados[i]
+
+def EliminacionNombre(Nombre,listaempleados):
+    for i in range(len(listaempleados)):
+        if Nombre==listaempleados[i].getnombre():
+            del listaempleados[i]        
+def EliminacionPrincipal(listaempleados):
+    ListarEmpleados(listaempleados)
+    MenuEliminarEmpleado()
+    opp=int(input(">> "))
+    if opp==1:
+        Numero=int(input("Ingrese el numero del Cliente"))
+        del listaEmpleados[Numero-1]
+    elif opp==2:
+        Nombre=input("Ingrese el nombre del Empleado")
+        EliminacionNombre(Nombre,listaempleados)
+    elif opp==3:
+        Ci=input("Ingrese el Numero de Carnet del Empleado")
+        EliminacionCi(Ci,listaempleados)    
+    else:
+        print("Opcion no valida")
+
+
+    
+    
+
+def ListarEmpleados(listaEmpleados):
+    for i in range(len(listaEmpleados)):
+        print("============================")
+        print("Cliente # ",(i+1))
+        
+        print("Nombre= ",listaEmpleados[i].getNombre())
+        print("Apellido= ",listaEmpleados[i].getApellido())
+        print("Direccion=",listaEmpleados[i].getDireccion())
+        print("Ci= ",listaEmpleados[i].getCi())
+        print("Fecha de Registro=",listaEmpleados[i].getFecha())
+        print("============================")
+           
+    input()
+def ModificarEmpleado(listaempleado):
+    ListarEmpleados(listaempleado)
+    
+    os.system("cls")
+    name=input("Ingrese el nombre Empleado")
+    apellid=input("Ingrese el apellido del empleado")
+    for i in listaempleado:
+        if i.getNombre()==name and i.getApellido()==apellid:
+            nombre=input()
+            apellido=input()
+            direccion=input()
+            ci=input()
+            fecha=input()
+        if nombre!=None:
+            i.setNombre(nombre)
+            if apellido!=None:
+                i.setApellido(apellido)
+            if direccion!=None:
+                i.setDireccion(direccion)
+            if ci!=None:
+                i.setCi(ci)
+            if fecha!=None:
+                i.setFecha(fecha) 
+        else:
+            print("Empleado no Encontrado")           
+
+
+
+
+
+    
+
+opcion=1
+listaEmpleados=[]
+
+while opcion!=0:
+    menu()
+    opcion=int(input("\t >> "))
+    if opcion==1:
+        os.system("cls")
+        Registrar(listaEmpleados)
+    if opcion==2:
+        os.system("cls")
+        EliminacionPrincipal(listaEmpleados)    
+    elif opcion==3:
+        os.system("cls")
+        ListarEmpleados(listaEmpleados)
+    elif opcion==4:
+        ModificarEmpleado(listaEmpleados)    
+    elif opcion==0:
+        os.system("cls")
+        print("Espere el sistema esta guardando datos")
+    else:
+        print("No existe es opcion!!!")
+        
+        
